b7eb9fe0098d116c11bfdd97330f77a54d9fec45
RealTribn <tristan.vandermeer@gmail.com>
2025-05-30 15:49:29 +0100

    new! tested

---
+++ downloader.py
@@ -0,0 +1,14 @@
+# Wow !
+import requests
+import re
+
+def steal(url):
+
+    filename = url.split("/")[-1]  
+    stripped = re.sub(r'^\d+-', '', filename) 
+
+    response = requests.get(url)
+    with open(filename, "wb") as file:
+        file.write(response.content)
+
+#steal("https://www.ocr.org.uk/Images/677968-mark-scheme-exploring-effects-and-impact.pdf") <-- test
