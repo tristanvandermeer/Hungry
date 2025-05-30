6d02abe9efbe3a1074052fc94ea08bde563702a6
RealTribn <tristan.vandermeer@gmail.com>
2025-05-30 15:58:18 +0100

    ranomd

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import random
 import time
 
 
+
 def main():
     with open("compare_links.txt", "r") as file:
         for url in file:
 
             print(f"DOWNLOADING: {url}")
             downloader.steal(url.rstrip())
+
+def RandomWaitTime():
+    pause = random.randrange(0, 1)
+    return pause
+
+print(RandomWaitTime)
